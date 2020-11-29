cbab208dde8587a2d8ff0e389d0e9b7cbd25e0eb
David Huang <hwhuang@sfu.ca>
2020-11-28 18:23:10 -0800

    knn indices

--- LOF.py
+++ LOF.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-# Calculate distances between every 2 data points
+# Calculate euclidean distances between every 2 data points
 def distances(data):
     dist = data.apply(lambda row: [np.linalg.norm(row.values - data.loc[[elem], :].values, 2) 
                                    for elem in data.index.values], axis=1)
@@ -16,39 +16,45 @@ def distances(data):
         index=data.index.tolist())
     return dist.to_numpy()
 
-# Get kth closest neighbors of each data point
-def knn_dist(data, dist, k):
-    knn = np.zeros(shape=(len(data), k))
-    knn_points = np.zeros(shape=(len(data), k))
+# Get kth closest neighbors (and their indices) of each data point
+def knn_distance(data, dist, k):
+    knn = np.zeros(shape = (len(data), k))
+    knn_index = np.zeros(shape = (len(data), k))
     for row in range(len(dist)):
         temp = dist[row]
-        temp = np.sort(temp)
-        knn[row] = temp[1:k+1]
-    return knn
+        temp2 = np.sort(temp)
+        temp3 = np.argsort(temp)
+        knn[row] = temp2[1:k+1]
+        knn_index[row] = temp3[1:k+1]
+    return knn, knn_index
 
 # Calculate average reachability for each point
-def avg_reachability(dist, knn, k):
+def avg_reachability(dist, knn, knn_idx, k):
     reach = np.zeros(len(dist))
+    print(len(knn))
     '''
-    for i in range(len(reach)):
-        for j in range(len(reach)):
-            if i != j:
-                return reach
+    for i in range(len(knn)):
+        avgRD = 0
+        for j in range(k):
+            if knn:
+                knn_dist = 0
+                eucl_dist = 0
+                
+                avgRD += max(knn_dist, eucl_dist)
         
-        #reach[i] = (1/k) *
+        reach[i] = avgRD / k
     '''
-    
     return reach
 
 # Returns outliers from input
 def local_outlier_factor(data, k):
     outliers = data.copy()
     dist = distances(data)
-    print(dist)
-    knn = knn_dist(data, dist, k)
+    knn, knn_idx = knn_distance(data, dist, k)
     print(knn)
-    reach = avg_reachability(dist, knn, k)
-    print(reach)
+    print(knn_idx)
+    reach = avg_reachability(dist, knn, knn_idx, k)
+    #print(reach)
     
     return outliers
 
@@ -56,9 +62,11 @@ def main():
     data = pd.read_csv("outliers-3.csv")
     outliers = local_outlier_factor(data, 3)
     
-    plt.scatter(data['X1'], data['X2'], s=5, c='b')
-    plt.scatter(outliers['X1'], outliers['X2'], s=5, c='r')
-    plt.show()
+    
+    
+    #plt.scatter(data['X1'], data['X2'], s=5, c='b')
+    #plt.scatter(outliers['X1'], outliers['X2'], s=5, c='r')
+    #plt.show()
     
 if __name__ == '__main__':
     main()
